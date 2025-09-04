f0eec62aa597a89e3952e182ccb23001b91b0804
Simon Smilga <simon0x1800@gmail.com>
2025-09-04 11:36:21 -0600

    feat: update package.json and add new dependencies for enhanced functionality
    feat: create seed script to populate database with crypto-related posts
    
    feat: implement PostFetcher component to display fetched posts
    
    feat: add API route for fetching posts from the database
    
    style: enhance global styles with Tailwind CSS and custom properties
    
    refactor: simplify Home component to render FetchPost directly
    
    feat: create reusable Card component for displaying post information
    
    feat: add utility functions for class name management
    
    fix: update tsconfig to allow emitting output files

---
+++ src/app/api/fetchpost/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const postfetcher = await prisma.post.findMany({
+        select:{
+            title:true,
+            content:true,
+            source:true,
+            signalTime:true,
+            published:true,
+            sentiment:true,
+        }
+    });
+    if(postfetcher.length === 0){
+        return NextResponse.json({error:"No Posts Found"},{status: 404})
+    }
+    return NextResponse.json(postfetcher,{status:200})
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
